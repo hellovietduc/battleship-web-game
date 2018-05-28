c9cbdcc497fad70dc648596649c5ba89e7cf1470
Nguyễn Việt Đức <vietduc01100001@gmail.com>
2018-05-28 21:29:45 +0700

    Add graphics to the game

--- js/main.js
+++ js/main.js
@@ -1,61 +1,112 @@
-let shipLocation = [];
-let isSunk = false;
-let message;
+const shipLocation = [];
+let isSunk = true;
+let guesses;
 
-window.onload = () => {
-    locateShip();
-    while (!isSunk) {
-        alert(message);
-        let userGuess = getUserGuess();
-        let hit = checkGuess(userGuess);
+$(document).ready(() => {
+    showInstruction(true);
+    showPlayGround(false);
+})
+
+// Play button handler
+// Can only be pressed when game ends
+$("#play").click(() => {
+    if (isSunk) {
+        showInstruction(false);
+        showPlayGround(true);
+        initGame();
+    }
+});
+
+// Restart button handler
+// Can be pressed anytime
+$("#restart").click(() => {
+    shipLocation.splice(0);
+    initGame();
+});
+
+// Boxes handler
+// Can only be pressed when in game
+$(".box").click(function () {
+    if (!isSunk) {
+        const userGuess = parseInt(this.id.charAt(4));
+        guesses++;
+        updateNumOfGuesses();
+
+        const hit = checkGuess(userGuess);
         if (hit) {
-            markShipAsHit(userGuess);
-            if (isShipSunk()) isSunk = true;
+            removeBrokenPart(userGuess);
+            markHitShip(userGuess);
+            if (isShipSunk()) {
+                isSunk = true;
+                showResult();
+            }
         }
     }
-    alert(message);
-};
+});
+
+/*** View functions ***/
+
+function showInstruction(show) {
+    const instructionDiv = $("#instruction");
+    if (show) instructionDiv.css("display", "block");
+    else instructionDiv.css("display", "none");
+}
+
+function showPlayGround(show) {
+    const playGroundDiv = $("#play-ground");
+    if (show) playGroundDiv.css("display", "block");
+    else playGroundDiv.css("display", "none");
+}
+
+function updateNumOfGuesses() {
+    $("#noti").text("Number of guesses: " + guesses);
+}
+
+function markHitShip(location) {
+    $("#col-" + location).removeClass("box").addClass("box-hit");
+}
+
+function unmarkAllHitShips() {
+    $(".box-hit").removeClass("box-hit").addClass("box");
+}
+
+function showResult() {
+    const accuracy = Math.round((3*100/guesses)*100)/100;
+    $("#noti").text("You won! Your accuracy is " + accuracy + "%.");
+}
+
+/*** Game logic functions ***/
+
+function initGame() {
+    locateShip();
+    isSunk = false;
+    guesses = 0;
+    updateNumOfGuesses();
+    unmarkAllHitShips();
+}
 
 function locateShip() {
-    let firstPoint = Math.round(Math.random()*10);
-    if (firstPoint + 2 > 9) {
+    const firstPoint = Math.floor(Math.random()*10); // Random number: 0-9
+    if (firstPoint + 2 > 9) { // Ship is out of range, locate it backwards
         shipLocation.push(firstPoint);
         shipLocation.push(firstPoint - 1);
         shipLocation.push(firstPoint - 2);
     }
-    else {
+    else { // Locate ship forwards
         shipLocation.push(firstPoint);
         shipLocation.push(firstPoint + 1);
         shipLocation.push(firstPoint + 2);
     }
-    message = "The ship has been located. Can you sink it?";
-}
-
-function getUserGuess() {
-    let guess;
-    do {
-        guess = parseInt(prompt("Guess the location of the ship (0-9):"));
-    } while (guess < 0 || guess > 9);
-    return guess;
 }
 
 function checkGuess(guess) {
-    if (shipLocation.includes(guess)) {
-        message = "You hit the ship!";
-        return true;
-    }
-    message = "You missed that!";
-    return false;
+    return shipLocation.includes(guess);
+}
+
+function removeBrokenPart(location) {
+    shipLocation.splice(shipLocation.indexOf(location), 1);
 }
 
 function isShipSunk() {
-    if (shipLocation.length === 0) {
-        message = "Ship sunk! You won!";
-        return true;
-    }
-    return false;
-}
-
-function markShipAsHit(location) {
-    shipLocation.splice(shipLocation.indexOf(location), 1);
+    return shipLocation.length === 0;
 }
